43766cecf0442d07e6e60f26bb2ede498c7c4005
Xiangrui2019 <xiangruikong2019@outlook.com>
2019-04-13 16:07:48 +0800

    VS Code Work

---
+++ Controllers/SendController.py
@@ -0,0 +1,35 @@
+from flask_script import Option
+from Library.Controller import Controller
+from Services.KahlaAuthApiService import KahlaAuthApiService
+from Services.StorageCookieService import StorageCookieService
+from Services.KahlaSignInStatusCheckService import KahlaSignInStatusCheckService
+from Services.KahlaFriendShipApiService import KahlaFriendShipApiService
+from Library.cryptojs import *
+import json
+
+class SendController(Controller):
+    def __init__(self):
+		self.friendshipservice = KahlaFriendShipApiService()
+		self.checkstatusservice = KahlaSignInStatusCheckService()
+
+    # 定义参数
+	def get_options(self):
+		return []
+
+	# 处理输入参数, 检查合法性
+	def run(self):
+		# 这条必须编写, 并且带上传入的参数
+		self.compute()
+
+	# 处理业务逻辑
+	def main(self):
+		if self.checkstatusservice.check() == True:
+			friends = self.friendshipservice.Friends()
+			friendsdata = json.loads(friends.text)["items"]
+			datas = []
+			for x in friendsdata:
+				pingdata = "{0} | {1}".format(x["displayName"], x["discriminator"])
+				datas.append(pingdata)
+			return datas
+		else:
+			return ["You are not logged in!"]
--- Services/KahlaConversationApiService.py
+++ Services/KahlaConversationApiService.py
@@ -6,7 +6,11 @@ from Services.StorageCookieService import StorageCookieService
 class KahlaConversationApiService(object):
     def __init__(self):
         self.apiaddress = KahlaApiAddressService()
-		self.storagecookie = StorageCookieService()
+        self.storagecookie = StorageCookieService()
     
     def ConversationDetail(self, conversationid):
-        requests.get("{0}/Conversation/ConversationDetail?id={1}".format(self.apiaddress))
+        r = requests.get("{0}/Conversation/ConversationDetail?id={1}".format(self.apiaddress.getaddress(), str(conversationid)), cookies=self.storagecookie.get())
+
+        return r
+
+    
