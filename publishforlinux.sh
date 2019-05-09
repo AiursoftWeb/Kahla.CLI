b1b5941d7acca880756a6f8adea247c2eccb176c
孔祥瑞 <48279726+Xiangrui2019@users.noreply.github.com>
2019-05-09 21:07:27 +0800

    Update publishforlinux.sh

--- publishforlinux.sh
+++ publishforlinux.sh
@@ -1,9 +1,9 @@
 rm -rf build
 rm -rf dist
-pyinstaller Program.py
+pyinstaller -F Program.py
 cp -r ./Controllers ./dist/Program/
 cp -r ./Library ./dist/Program/
 cp -r ./Listener ./dist/Program/
 cp -r ./Services ./dist/Psrogram/
 cp -r ./Views ./dist/Program/
-mv ./dist/Program/Program ./dist/Program/kahlaz
+mv ./dist/Program/Program ./dist/Program/kahla
