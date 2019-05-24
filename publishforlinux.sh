c3a4e85d5df6938fc044d50b08d7480e7b51a284
Xiangrui2019 <xiangruikong2019@outlook.com>
2019-05-23 22:19:06 -0700

    fix#49 fix the bug

--- publishforlinux.sh
+++ publishforlinux.sh
@@ -7,4 +7,5 @@ cp -r ./Library ./dist/Program/
 cp -r ./Listener ./dist/Program/
 cp -r ./Services ./dist/Psrogram/
 cp -r ./Views ./dist/Program/
+cp -r ./package.py ./dist/Program/package.py
 mv ./dist/Program/Program ./dist/Program/kahla
